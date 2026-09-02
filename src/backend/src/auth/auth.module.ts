418ada606aa0a906779a9530cf341d2f401d0944
Mateo Pineda <Mateo Pineda@DESKTOP-6PR1K67>
2026-09-01 19:39:06 -0500

    Completar el setup de JWT con el secret en el env y en constants.ts

--- src/backend/src/auth/auth.module.ts
+++ src/backend/src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller.js';
+import { AuthService } from './auth.service.js';
+import { UsersModule } from '../users/users.module.js';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants.js';
+
 
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
