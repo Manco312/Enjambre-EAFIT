638aa6bde044d23f06550425c7d5eca98cb5ad60
Mateo Pineda <Mateo Pineda@DESKTOP-6PR1K67>
2026-09-01 20:08:23 -0500

    Ajuste del módulo Users para terminar los endpoints (privados) de registrar usuarios y crear usuario admin de defecto.

--- src/backend/src/users/users.service.ts
+++ src/backend/src/users/users.service.ts
@@ -1,9 +1,8 @@
-
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException  } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from './entities/user.entity.js'
-
+import { User } from './entities/user.entity.js';
+import { CreateUserDto } from './dto/create-user.dto.js';
 
 @Injectable()
 export class UsersService {
@@ -13,6 +12,31 @@ export class UsersService {
   ) {}
 
   findOne(username: string): Promise<User | null> {
-    return this.usersRepository.findOneBy({username});
+    return this.usersRepository.findOneBy({ username });
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const existingUser = await this.findOne(createUserDto.username);
+
+    if (existingUser) {
+      throw new BadRequestException('Este nombre de usuario ya está en uso.');
+    }
+
+    const user = this.usersRepository.create({
+      ...createUserDto,
+      role: 'user',
+    });
+
+    return await this.usersRepository.save(user);
+  }
+
+  async createAdmin(): Promise<User> {
+    const admin = this.usersRepository.create({
+      username: process.env.ADMIN_USERNAME,
+      password: process.env.ADMIN_PASSWORD,
+      role: 'admin',
+    });
+
+    return await this.usersRepository.save(admin);
   }
 }
